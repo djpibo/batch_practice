6901cce863053dc5f68678546497d4ed312835c8
deejay95 <djdj0622@gmail.com>
2024-01-31 14:45:54 +0900

    init

---
+++ src/main/java/com/practice/batchPrj/BatchPrjApplication.java
@@ -0,0 +1,13 @@
+package com.practice.batchPrj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchPrjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/batchPrj/BatchPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.batchPrj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
